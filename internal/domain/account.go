31c70acecc22460f7851bda0fc015603f459ee7e
RianAbdias <rianabdias123@gmail.com>
2026-05-09 15:06:16 -0300

    feat: adiciona conta bonus

--- internal/domain/account.go
+++ internal/domain/account.go
@@ -1,6 +1,17 @@
 package domain
 
+const (
+	AccountTypeSimple = "simples"
+	AccountTypeBonus  = "bonus"
+)
+
 type Account struct {
 	Number  string  `json:"number"`
 	Balance float64 `json:"balance"`
+	Type    string  `json:"type"`
+	Points  int     `json:"points"`
+}
+
+func (a Account) IsBonus() bool {
+	return a.Type == AccountTypeBonus
 }
